70c9c4ff70878eb99145bbb6cc8c3ed5e744e337
Keigo TAKADA <116698979+Keig0@users.noreply.github.com>
2026-05-11 12:41:07 +0900

    変数、条件分岐、繰り返し

--- hello.rb
+++ hello.rb
@@ -1,3 +1,33 @@
 #! /usr/bin/env ruby
 
 puts "Hello, World!"
+a=5
+s="Ruby"
+d=3.14
+puts a
+puts s
+puts d
+puts "a:"+a.to_s()
+if a>5
+    puts "Large"
+elsif
+    puts "Negative"
+else
+    puts "Little"
+end
+
+x=3
+while x>0
+    puts "#"
+    x-=1
+end
+
+a.times do
+    print "*"
+end
+puts
+
+a.times do |i|
+    print i
+end
+puts
